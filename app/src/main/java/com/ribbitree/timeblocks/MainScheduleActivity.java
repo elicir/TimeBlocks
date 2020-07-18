d2d25a2e760b1e6f86855031ab181d85219e0743
elicir <ericaj.li@mail.utoronto.ca>
2020-07-18 18:00:05 -0400

    Renamed classes and set up onclick framework (didn't get it to select correct view yet)

--- app/src/main/java/com/ribbitree/timeblocks/MainScheduleActivity.java
+++ app/src/main/java/com/ribbitree/timeblocks/MainScheduleActivity.java
@@ -3,25 +3,34 @@ package com.ribbitree.timeblocks;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainScheduleActivity extends AppCompatActivity implements TimeRecyclerAdapter.OnTimeListener{
+
+    private ListData[] myListData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ListData[] myListData = new ListData[49];
+        myListData = new ListData[49];
         for (int i = 0; i < 48; i+=2) {
             myListData[i] = new ListData(i/2, false);
             myListData[i+1] = new ListData(i/2, true);
         }
         myListData[48] = new ListData(24, false);
 
-        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
-        MyAdapter adapter = new MyAdapter(myListData);
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        TimeRecyclerAdapter adapter = new TimeRecyclerAdapter(myListData, this);
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(adapter);
+    }
+
+    @Override
+    public void onTimeClick(int position) {
+        Toast.makeText(getApplicationContext(), "Clicked on: " + myListData[position].getTime(), Toast.LENGTH_SHORT).show();
     }
 }
---
+++ app/src/main/java/com/ribbitree/timeblocks/TimeRecyclerAdapter.java
@@ -0,0 +1,72 @@
+package com.ribbitree.timeblocks;
+
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+public class TimeRecyclerAdapter extends RecyclerView.Adapter<TimeRecyclerAdapter.ViewHolder>{
+
+    RecyclerView recyclerView;
+    private ListData[] listData;
+    private OnTimeListener onTimeListener;
+
+    public TimeRecyclerAdapter(ListData[] listData, OnTimeListener onTimeListener) {
+        this.listData = listData;
+        this.onTimeListener = onTimeListener;
+    }
+
+    @Override
+    public void onAttachedToRecyclerView(@NonNull RecyclerView recyclerView) {
+        super.onAttachedToRecyclerView(recyclerView);
+        this.recyclerView = recyclerView;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        View listItem= layoutInflater.inflate(R.layout.list_item, parent, false);
+        return new ViewHolder(listItem, onTimeListener, recyclerView);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        holder.timeView.setText(listData[position].getTime());
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return listData.length;
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        public TextView timeView;
+        public TextView blockView;
+        public OnTimeListener onTimeListener;
+        public RecyclerView recyclerView;
+        public ViewHolder(View itemView, OnTimeListener onTimeListener, RecyclerView recyclerView) {
+            super(itemView);
+            this.onTimeListener = onTimeListener;
+            this.timeView = itemView.findViewById(R.id.time);
+            this.blockView = itemView.findViewById(R.id.lineBg);
+            this.recyclerView = recyclerView;
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            onTimeListener.onTimeClick(getAdapterPosition());
+        }
+    }
+
+    public interface OnTimeListener{
+        void onTimeClick(int position);
+    }
+}
