8493b31298fce7daf0fb54d7a092035d1bcf91b5
elicir <ericaj.li@mail.utoronto.ca>
2020-10-17 00:49:41 -0400

    Limited text length to the end of the entry edittext box w/ padding

--- app/src/main/java/com/ribbitree/timeblocks/MainActivity.java
+++ app/src/main/java/com/ribbitree/timeblocks/MainActivity.java
@@ -4,9 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 
 import android.app.Activity;
+import android.graphics.Paint;
+import android.graphics.Rect;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.inputmethod.BaseInputConnection;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -76,6 +81,28 @@ public class MainActivity extends AppCompatActivity implements EntryAdapter.OnIt
             public void onFocusChange(View view, boolean hasFocus) {
                 if (!hasFocus) {
                     saveEntry(blockEntry, entry);
+                }
+            }
+        });
+        entry.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                Rect bounds = new Rect();
+                Paint textPaint = entry.getPaint();
+                textPaint.getTextBounds(entry.getText().toString(), 0, entry.getText().toString().length(), bounds);
+                int width = bounds.width();
+                final float dpi = getBaseContext().getResources().getDisplayMetrics().density;
+                BaseInputConnection textFieldInputConnection = new BaseInputConnection(entry, true);
+                if (width > entry.getMeasuredWidth() - 16 * dpi) {
+                    textFieldInputConnection.sendKeyEvent(new KeyEvent(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_DEL));
                 }
             }
         });
